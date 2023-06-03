4f51cebd9bbaaab5176049beabd1f4fcd871ebf6
farhan <Www.mfarvirus@gmail.com>
2023-06-04 01:01:29 +0500

    base template

---
+++ src/routes/+page.svelte
@@ -0,0 +1,27 @@
+
+    <!-- src/routes/+page.svelte -->
+
+    <script lang="ts">
+        import type { PageServerData } from './$types';
+
+        export let data: PageServerData;
+
+        console.log(data, "data");
+    </script>
+
+    <div class="container">
+        <h1>Markdown Blog</h1>
+
+        <ul class="links">
+            {#each data.posts as post}
+            <li>
+                <a href={`/post/${post.slug}`}>{post.title}</a>
+            </li>
+            {/each}
+        </ul>
+    </div>
+
+    <style>
+      
+    </style>
+
